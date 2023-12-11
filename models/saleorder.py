4d04d501a8217d16f781c1224d2ae4dc6827cf51
Febry Ramadhan <febryramadhan285@gmail.com>
2023-12-11 14:48:44 +0700

    buat seprti advertise

--- models/saleorder.py
+++ models/saleorder.py
@@ -6,4 +6,7 @@ class SaleOrder(models.Model):
     sirkulasi_ok = fields.Boolean('Sirkulasi Product')
     rekap_so_ids = fields.One2many(comodel_name="so_sirkulasi.rekap_so", inverse_name='rekap_so_id')
 
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
     
+    sirkulasi_ok = fields.Boolean('Sirkulasi Product')
